8f2c08e61beeec33bd8c22da02981f531d55eff8
Yongcheng Wu <yc_wu1030@126.com>
2024-01-08 23:59:39 +0800

    feat(SM): add quark masses and CKM matrix

--- include/SM.h
+++ include/SM.h
@@ -1,40 +1,52 @@
 #ifndef GM_FCNC_SM_H_
 #define GM_FCNC_SM_H_
 
+#include <complex>
+
 #include "Constants.h"
 
 class SM {
 public:
+    typedef unsigned Generation_t;
     SM();
 
     double get_MZ() const { return MZ; }
     double get_MZ2() const { return MZ2; }
-    double get_MW() const { return MW; }
-    double get_MW2() const { return MW2; }
-    double get_CW() const { return CW; }
-    double get_CW2() const { return CW2; }
-    double get_SW() const { return SW; }
-    double get_SW2() const { return SW2; }
+    double get_MW() const { return m_MW; }
+    double get_MW2() const { return m_MW2; }
+    double get_CW() const { return m_CW; }
+    double get_CW2() const { return m_CW2; }
+    double get_SW() const { return m_SW; }
+    double get_SW2() const { return m_SW2; }
     double get_MT() const { return MT; }
     double get_MT2() const { return MT2; }
-    double get_gweak() const { return g_weak; }
-    double get_gphyper() const { return gp_hyper; }
-    double get_vev() const { return vev; }
+    double get_gweak() const { return m_g_weak; }
+    double get_gphyper() const { return m_gp_hyper; }
+    double get_vev() const { return m_vev; }
+
+    double get_m_up(Generation_t i) const { return m_m_ups[i]; }
+    double get_m_down(Generation_t i) const { return m_m_downs[i]; }
+    std::complex<double> get_vckm(Generation_t i, Generation_t j) const { return m_VCKM[i][j]; }
+    std::complex<double> get_vckmc(Generation_t i, Generation_t j) const { return std::conj(m_VCKM[i][j]); }
 
 protected:
-    double MW;
-    double MW2;
-    double thetaW;
-    double SW;
-    double SW2;
-    double CW;
-    double CW2;
-    double alpha;
-    double vev;       // * (sqrt(2)GF)^(-0.5)
-    double EL;        // * EL = sqrt(4*Pi*alpha)
-    double g_weak;    // * g = EL/SW
-    double gp_hyper;  // * gp = EL/CW
-    double yt;        // * sqrt(2)mt
+    double m_MW;
+    double m_MW2;
+    double m_thetaW;
+    double m_SW;
+    double m_SW2;
+    double m_CW;
+    double m_CW2;
+    double m_alpha;
+    double m_vev;       // * (sqrt(2)GF)^(-0.5)
+    double m_EL;        // * EL = sqrt(4*Pi*alpha)
+    double m_g_weak;    // * g = EL/SW
+    double m_gp_hyper;  // * gp = EL/CW
+    double m_yt;        // * sqrt(2)mt/vev
+
+    double m_m_ups[3];
+    double m_m_downs[3];
+    std::complex<double> m_VCKM[3][3];
 };
 
 #endif  // GM_FCNC_SM_H_
